aa7e1eea173075e17258283f671e64ef6af3029f
Abhay Jha <54393533+AJ-Walker@users.noreply.github.com>
2020-03-15 21:12:17 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,20 @@
+﻿import socket
+
+# creating a socket object
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# get local machine name
+
+host = socket.gethostname()
+port = 9999
+
+# connection to hostname on the port
+
+serv.connect((host, port))
+
+# receive no more than 1024 bytes
+
+msg = serv.recv(1024)
+serv.close()
+print msg.decode('ascii')
---
+++ server.py
@@ -0,0 +1,28 @@
+﻿import socket
+
+# creating a socket object
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Get local machine name
+
+host = socket.gethostname()
+print host
+port = 9999
+
+# Bind to the port
+
+serversocket.bind((host, port))
+
+# queue upto 5 requests
+
+serversocket.listen(5)
+while True:
+
+    # establish a connection
+
+    (clientsocket, addr) = serversocket.accept()
+    print 'Geta connection from%s' % str(addr)
+    msg = 'Thank you for connecting' + '\r\n'
+    clientsocket.send(msg.encode('ascii'))
+    clientsocket.close()
